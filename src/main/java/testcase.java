e9055ed838fc1dece847ab853bca527e9aec281e
Administrator <1813561379@qq.com>
2020-03-21 15:42:20 +0800

    first commit

---
+++ src/main/java/testcase.java
@@ -0,0 +1,44 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+
+public class testcase {
+    private WebDriver driver;
+    private String url = "https://www.baidu.com";
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @BeforeMethod
+    public void setUp() {
+            String browserDriverPath = "C:\\Users\\Administrator\\IdeaProjects\\Automated_UI\\src\\main\\resources\\chromedriver.exe";
+            System.setProperty("webdriver.chrome.driver",browserDriverPath);
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("start-maximized");
+            driver = new ChromeDriver(options);
+//            driver = new ChromeDriver();
+//            driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testLogin() {
+        driver.get(url);
+        driver.findElement(By.id("kw")).clear();// 按id找到元素后，清空该元素
+        driver.findElement(By.id("kw")).sendKeys("selenium");// 输入selenium
+        driver.findElement(By.id("su")).click(); //点击搜索按钮
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+                Assert.fail(verificationErrorString);
+        }
+    }
+}
