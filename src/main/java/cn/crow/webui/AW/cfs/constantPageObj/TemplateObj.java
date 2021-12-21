9180916ea278367ffff3ba2a00deb10d8e851da6
crow <1813561379@qq.com>
2021-12-21 18:18:36 +0800

    默认更改列表

--- src/main/java/cn/crow/webui/AW/cfs/constantPageObj/TemplateObj.java
+++
@@ -1,9 +0,0 @@
-package cn.crow.webui.AW.cfs.constantPageObj;
-
-public class TemplateObj {
-
-    //+新建button
-    public static final String xinjian_xpath = "//*[@id=\"root\"]//span[contains(text(),\"+新建\")]";
-
-    //
-}
